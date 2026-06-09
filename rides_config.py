78bcb3f4350e812f90d7c20a53a09d2ea9435ad6
lpjr-parksignals <lpjrparksignals@gmail.com>
2026-06-08 23:25:00 -0500

    Update rides_config.py

--- rides_config.py
+++ rides_config.py
@@ -1,9 +1,11 @@
-MAJOR_RIDES = [
+MAJOR_RIDES = {
     "TRON Lightcycle / Run",
     "Space Mountain",
     "Seven Dwarfs Mine Train",
     "Big Thunder Mountain Railroad",
-    "Pirates of the Caribbean",
     "Haunted Mansion",
+    "Pirates of the Caribbean",
     "Jungle Cruise",
-]
+    "Peter Pan's Flight",
+    "Tiana's Bayou Adventure",
+}
